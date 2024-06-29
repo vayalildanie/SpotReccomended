b6ace309144b753ba75bda5502bfc3a898a983b7
Pierce <spammermuch@gmail.com>
2024-06-29 15:11:58 -0700

    last backup

--- frontend/my-app/src/components/Chat.tsx
+++ frontend/my-app/src/components/Chat.tsx
@@ -1,9 +1,31 @@
+import React, { useEffect, useState } from 'react'
+
+import './Chat.css'
+
 function Chat() {
+  let [ messages, setMessages ] = useState<string[]>([])
+  useEffect(() => {
+    // setupListener()
+  }, [messages])
   return (
-    <div>
+    <div id='chat'>
       <h1>Chat test</h1>
+      <div id='history'>
+      {messages.map((message, index) => <p key={index}>{message}</p>)}
+        <input type='text' id='message' />
+        <button onClick={() => { sendMessage(setMessages) }}>Send</button>
+      </div>
     </div>
   )
+}
+
+function setupListener() {
+  throw new Error('Function not implemented.');
+}
+
+function sendMessage(setMessages: React.Dispatch<React.SetStateAction<string[]>>) {
+  const message = document.getElementById('message') as HTMLInputElement
+  setMessages(prev => [...prev, message.value])
 }
 
 export default Chat;
